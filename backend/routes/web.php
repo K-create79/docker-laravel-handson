bd44c27103b09b5bb7d7488707996e44215bdae6
tanaka <gantaishanpu@gmail.com>
2021-05-01 14:07:52 +0900

    feat create hello.blade.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    logger('welcome route.');
     return view('welcome');
+});
+
+Route::get('/hello', function() {
+    return view('hello');
 });
