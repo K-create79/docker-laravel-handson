c71313d46503f43fd0dd7cdcedbcbe6556523416
sugiurakanta <gantaishanpu3@gmail.com>
2021-05-21 10:32:57 +0900

    create show.balde.php communicated memo.css

--- backend/resources/views/memo/showMemos.blade.php
+++ backend/resources/views/memo/showMemos.blade.php
@@ -7,12 +7,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>メモ一覧</title>
-  <link rel="stylesheet" href="./../../css/app.css">
+  <link href="css/memo.css" rel="stylesheet">
 </head>
 <body>
 <a href="/createMemo">メモ作成</a>
 
-<h1>メモ一覧</h1>
+<h1>メモ一覧</h1> 
 <table >
   <tr>
     <th>タイトル</th>
